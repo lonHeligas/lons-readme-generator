9300eea34607736335d51e2d7d716ba8d7ca42b3
Lon Heligas <lon.heligas@gmail.com>
2023-01-22 15:12:41 -0600

    setup of variables

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+var inquirer = require('inquirer');
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = {
+  readmeTitle,
+  readmeDesc,
+  readmeInst,
+  readmeUsage,
+  readmeGuidelines,
+  readmeTest,
+  readmeLicense,
+  readmeUsername,
+  readmeEmail,
+
+
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +27,22 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+/* todo 
+    title
+    description
+    installation inst
+    usage info
+    contribution guidelines
+    test inst
+    license choice
+    Github username (with link to my profile)
+    email address
+    
+    click on links in table of contents to take me to each sectdion
+
+*/
+
