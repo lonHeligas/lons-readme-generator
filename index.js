d83a867e9b2679ca17540c1f9cef6376e4a92ac5
Lon Heligas <lon.heligas@gmail.com>
2023-01-24 16:43:47 -0600

    added license icons. haven't tested yet

--- index.js
+++ index.js
@@ -2,82 +2,129 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
       {
         type: 'input',
-        message: 'Enter your README\'s Title:',
         name: 'readmeTitle',
+        message: 'Enter your README\'s Title:',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs a title.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
-        message: 'Enter your README\'s Description',
         name: 'readmeDesc',
+        message: 'Enter your README\'s Description',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs a description.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
-        message: 'Enter your README\'s Installation Instructions',
         name: 'readmeInst',
+        message: 'Enter your README\'s Installation Instructions',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs a installation instructions.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
-        message: 'Enter your README\'s Usage Information',
         name: 'readmeUsage',
+        message: 'Enter your README\'s Usage Information',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs usage information.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
-        message: 'Enter your README\'s Contribution Guidlines',
         name: 'readmeGuidelines',
+        message: 'Enter your README\'s Contribution Guidlines',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs contribution guidelines.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
-        message: 'Enter your README\'s Testing Guidlines',
         name: 'readmeTest',
+        message: 'Enter your README\'s Testing Guidlines',
+        validate: (response) => {
+          if (response === ''){
+            return 'Your README still needs testing guidelines.'
+          }
+          return true
+        }
       },
       {
         type: 'list',
+        name: 'license',
         message: 'Choose your license for your applicaiton:',
-        name: 'readmeLicense',
         choices: [
           {name: 'apacheLi', value: 'Apache 2.0'},          
           {name: 'gnuLi', value: 'GNU GPL V3'},
           {name: 'mitLi',value: 'The MIT License'},
           {name: 'mozillaLi', value: ' Mozilla Public License 2.00'},
-        ]
-
-        /*
-          Apache 2.0          
-          GNU GPL v3
-          The MIT License
-          Mozilla Public License 2.00
-        */
+        ],
+        default: 'mitLi'
       },
       {
         type: 'input',
         message: 'Enter your GitHub username (URL only, no http://):',
         name: 'readmeUsername',
+        validate: (response) => {
+          if (response === ''){
+            return 'I need your GitHub username, please.'
+          }
+          return true
+        }
       },
       {
         type: 'input',
         message: 'Enter your email address:',
         name: 'readmeEmail',
+        validate: (response) => {
+          if (response === ''){
+            return 'I need your email address please. (I promise I will send you no spam.)'
+          }
+          return true
+        }
       },
     ];
-  
- 
+    
+    
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  //fs.writeFile(filename, data, (err) =>)
+}
 
 // TODO: Create a function to initialize app
-function init() {
-  
+function init() {  
   const init = async () =>{
     let answers = await inquirer.prompt(questions)
+
+
+    // writetofile(`./README.md`, generateMarkdown(answers)); ??
   }
   
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,21 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
+  let licenseIcon;
+  switch (license) {
+    case 'Apache 2.0':
+      licenseIcon = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'gnuLi':
+      licenseIcon = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'mitLi':
+      licenseIcon = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'mozillaLi':
+      licenseIcon = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+  }
 /*
   Apache 2.0
   [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) 
@@ -46,21 +61,6 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
-/*
-  Apache 2.0
-  https://choosealicense.com/licenses/apache-2.0/
-
-  GNU GPL v3  
-  https://choosealicense.com/licenses/gpl-3.0/
-
-  The MIT License 
-  "https://choosealicense.com/licenses/mit/"
-
-  Mozilla Public License 2.00  
-  https://choosealicense.com/licenses/mpl-2.0/
-*/
-
-
 
 
 
